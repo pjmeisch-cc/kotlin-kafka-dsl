55929bc5fbbd03225c69f490c4c8948b544ed5ea
P.J. Meisch <peter-josef.meisch@codecentric.de>
2018-06-19 12:08:42 +0200

    add no-DSL implementation

---
+++ src/main/kotlin/de/codecentric/Kafka.kt
@@ -0,0 +1,66 @@
+package de.codecentric
+
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.kafka.common.serialization.StringSerializer
+import java.util.*
+
+/**
+ * @author P.J. Meisch (peter-josef.meisch@codecentric.de)
+ */
+
+class Kafka(val bootstrapServers: String)
+
+class Producer(kafka: Kafka, private val topic: String) {
+    private val kafkaProducer: KafkaProducer<String, String>
+
+    init {
+        val config = Properties()
+        config[ProducerConfig.BOOTSTRAP_SERVERS_CONFIG] = kafka.bootstrapServers
+        config[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
+        config[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
+        kafkaProducer = KafkaProducer(config)
+    }
+
+    fun send(msg: String) {
+        kafkaProducer.send(ProducerRecord(topic, msg))
+    }
+
+    fun flush() = kafkaProducer.flush()
+}
+
+class Consumer(kafka: Kafka, topic: String) {
+    private val kafkaConsumer: KafkaConsumer<String, String>
+
+    @Volatile
+    var keepGoing = true
+
+    init {
+        val config = Properties()
+        config[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] = kafka.bootstrapServers
+        config[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java
+        config[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java
+        config[ConsumerConfig.GROUP_ID_CONFIG] = UUID.randomUUID().toString()
+        kafkaConsumer = KafkaConsumer(config)
+        kafkaConsumer.subscribe(listOf(topic))
+    }
+
+    fun consume(onMessage: (value: String) -> Unit) = Thread(Runnable {
+        keepGoing = true
+        kafkaConsumer.use { kc ->
+            while (keepGoing) {
+                kc.poll(500)?.forEach {
+                    onMessage(it?.value() ?: "???")
+                }
+            }
+        }
+    }).start()
+
+    fun stop() {
+        keepGoing = false
+    }
+}
--- src/main/kotlin/de/codecentric/Runner.kt
+++ src/main/kotlin/de/codecentric/Runner.kt
@@ -1,9 +1,59 @@
 package de.codecentric
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import java.time.LocalDateTime
+
 /**
  * @author P.J. Meisch (peter-josef.meisch@codecentric.de)
  */
 
+val log: Logger = LoggerFactory.getLogger("de.codecentric.Runner")
+
 fun main(args: Array<String>) {
-    println("Runner")
+
+    log.info("starting up")
+
+    // setup  Kafka config
+    val bootstrapServers = "localhost:9092"
+    val kafka = Kafka(bootstrapServers)
+
+    val topic = "kt-topic"
+
+    // start a Thread with a Consumer
+    val tc = Thread(consumerRunnable(kafka, topic))
+    tc.start()
+
+    // give the consumer a little time to start
+    Thread.sleep(300)
+
+    // now start the Producer thread
+    val tp = Thread(producerRunnable(kafka, topic))
+    tp.start()
+
+    tp.join()
+
+    // consumer will be shutdown on program termination (ctrl-c)
+    tc.join()
 }
+
+private fun consumerRunnable(kafka: Kafka, topic: String) = Runnable {
+    val consumer = Consumer(kafka, topic)
+    Runtime.getRuntime().addShutdownHook(Thread(Runnable {
+        consumer.stop()
+    }))
+    consumer.consume {
+        log.info("got $it")
+    }
+}
+
+private fun producerRunnable(kafka: Kafka, topic: String) = Runnable {
+    val producer = Producer(kafka, topic)
+    (1..10).forEach {
+        val msg = "test message $it ${LocalDateTime.now()}"
+        log.info("sending $msg")
+        producer.send(msg)
+    }
+    producer.flush()
+}
+
